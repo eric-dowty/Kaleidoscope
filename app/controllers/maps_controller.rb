8bf1d05e10bcf919ab6851e9e48d3a812cd69829
end3rs-army <eric.m.dowty@gmail.com>
2015-06-03 00:09:53 -0600

    prepare to have your minds blown... added functionality to /maps

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,9 +1,10 @@
 class MapsController < ApplicationController
+  
   def index
-    @geo_json = InstagramData.get_json_map_data
+    @geojson = InstagramData.get_json_map_data
   end
 
   def show
-
   end
+
 end
