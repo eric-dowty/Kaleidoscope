a7887d41d30e861036ab2e4b6e99d4fb41a1248f
end3rs-army <eric.m.dowty@gmail.com>
2015-06-07 21:59:16 -0600

    added method for finding most popular hash tags

--- app/controllers/home_controller.rb
+++
@@ -1,8 +0,0 @@
-class HomeController < ApplicationController
-  def index
-  end
-
-  def instagram
-    @media = Instagram.media_search("39.740567", "-104.988746", options ={ count: 100})
-  end
-end
