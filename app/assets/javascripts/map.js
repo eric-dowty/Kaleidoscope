e2ce355275f6142056d86d68229f2200afac543f
end3rs-army <eric.m.dowty@gmail.com>
2015-06-08 21:19:17 -0600

    cleaned up the javascript as best I could

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,61 +1,38 @@
 $(document).ready(function(){
 
+//Set mapbox map view
 L.mapbox.accessToken = $('#map-data').data('token')
- 
-window.geojsonData = {}
+var map              = L.mapbox.map('map', 'boomkenster.mbi8c0ap')
+var markerLayer      = L.mapbox.featureLayer().addTo(map);
+var turing           = [39.750081, -104.999703];
+map.setView(turing, 13);
 
-var addRow = function(index, data){
-    $("#instagram-table").append("<tr class='instagram-row' data-index="+index+
-                              "><td class='instagram-thumb'><img src=" + data["properties"]["thumbnail"] + 
-                              " alt='instagram picture' /><br><button class='btn btn-collapse' type='button' data-toggle='collapse' data-target='#post-data" + 
-                              index + "' aria-expanded='false' aria-controls='post-data" + index + 
-                              "'><span class='glyphicon glyphicon-chevron-down' aria-hidden='true'></span></button><div class='collapse' id='post-data" + index +
-                              "'><div class='instagram-username'>" +data["properties"]["username"] + 
-                              "<br></div><div class='instagram-post'>" + data["properties"]["post"] + 
-                              "<br></div><div class='instagram-likes'>Likes:" + data["properties"]["likes"] +
-                              "</div></div></td></tr>")
+var rowHTML = function(index, data) {
+  return("<tr class='instagram-row' data-index="+index+">"
+         + "<td class='instagram-thumb'>"
+         +  "<img src="+data["properties"]["thumbnail"]+" alt='instagram picture' /><br>"
+         +  "<button class='btn btn-collapse' type='button' data-toggle='collapse' data-target='#post-data"+index+"' aria-expanded='false' aria-controls='post-data"+index+"'>"
+         +  "<span class='glyphicon glyphicon-chevron-down' aria-hidden='true'></span>"
+         +  "</button><div class='collapse' id='post-data"+index+"'>"
+         +  "<div class='instagram-username'>"+data["properties"]["username"]+"<br>"
+         +  "</div><div class='instagram-post'>"+data["properties"]["post"]+"<br>"
+         +  "</div><div class='instagram-likes'>Likes:"+data["properties"]["likes"]+"</div></div>"
+         + "</td></tr>")
 }
 
-var table = document.getElementById("instagram-table")
+var addRows = function(data){
+  $('#instagram-table').html("");
+  var htmlData = ""
+  for(i=1; i<data.length; i++){
+    htmlData = htmlData + rowHTML(i, data[i])
+  };
+  $("#instagram-table").append(htmlData)
+}
 
-var createPoints = function(coordinates){
-  var url = '/maps.json'
-  if (coordinates !== undefined) {
-    url += '?lat=' + coordinates['lat'] + '&lon=' + coordinates['lng'];
-   }
+var mapHighlight = function(data){
 
-    $.get(url, function(data) {
-      window.markerLayer = L.mapbox.featureLayer().addTo(map);
-      window.geojsonData = data
-      markerLayer.setGeoJSON(data);
-
-      markerLayer.on('mouseover', function(e) {
-        var marker  = e.layer;
-        popUpAll(marker);
-      });
-
-      markerLayer.on('mouseout', function(e) {
-        e.layer.closePopup();
-      });
-
-      highlightLayer.on('mouseover', function(z) {
-        var marker  = z.layer;
-        popUpAll(marker);
-      });
-
-      highlightLayer.on('mouseout', function(z) {
-        z.layer.closePopup();
-      });
-
-      table.innerHTML = "";
-      for(i=1; i<data.length;i++){
-
-        addRow(i, data[i])
-      };
-
-        $(".instagram-row").mouseenter(function() {
-    console.log("enter")
-    if ($(this).data('index') === 0 ){
+  $(".instagram-row").mouseenter(function() {
+    if ($(this).data('index') === 1 ){
       $(this).addClass('row-one-higlight')  
     } else {
       $(this).addClass('row-highlight')
@@ -64,8 +41,7 @@ var createPoints = function(coordinates){
   });
 
   $('.instagram-row').mouseleave(function() {
-    console.log("leave")
-    if ($(this).data('index') === 0 ){
+    if ($(this).data('index') === 1 ){
       $(this).removeClass('row-one-higlight')  
     } else {
       $(this).removeClass('row-highlight')
@@ -73,21 +49,39 @@ var createPoints = function(coordinates){
     resetMarker($(this).data('index'))
   });
 
-
   var highlightMarker = function(index) {
-    var instagramMarker = geojsonData[index]
+    var instagramMarker = data[index]
     instagramMarker.properties['marker-color'] = "#B502CD";
     instagramMarker.properties['marker-size']  = 'large';
-    markerLayer.setGeoJSON(geojsonData);
-    highlightLayer.setGeoJSON(instagramMarker);
+    markerLayer.setGeoJSON(data);
   }
 
   var resetMarker = function(index) {
-    var instagramMarker = geojsonData[index]
+    var instagramMarker = data[index]
     instagramMarker.properties['marker-color'] = '#000000';
     instagramMarker.properties['marker-size']  = 'small';
   }
+}
 
+var createPoints = function(coordinates){
+  var url = '/maps.json'
+  if (coordinates !== undefined) {
+    url += '?lat=' + coordinates['lat'] + '&lon=' + coordinates['lng'];
+  }
+  $.get(url, function(data) {
+  markerLayer.on('mouseover', function(e) {
+    var marker  = e.layer;
+    popUpAll(marker);
+  });
+
+  markerLayer.on('mouseout', function(e) {
+    e.layer.closePopup();
+  });
+  
+  addRows(data)
+  mapHighlight(data)
+  
+  markerLayer.setGeoJSON(data);
   });
 
   var popUpAll = function(marker){
@@ -101,14 +95,9 @@ var createPoints = function(coordinates){
 
     marker.openPopup();
   }
+
 };
 
-var turing = [39.750081, -104.999703];
-
-
-window.map = L.mapbox.map('map', 'boomkenster.mbi8c0ap')
-
-map.setView(turing, 13);
 
 $("#location-btn").on('click', function(){
     map.locate();
@@ -119,15 +108,24 @@ map.on('locationfound', function(e) {
 
 var popup = L.popup();
 
-function onMapClick(e) {
+var onMapClick = function(e){
     popup
         .setLatLng(e.latlng)
         .setContent("Loading up the pix!")
         .openOn(map);
-        map.removeLayer(markerLayer);
+        markerLayer.clearLayers()
         createPoints(e.latlng);
 }
+
 map.on('click', onMapClick);
+
+// Create points on page load
+createPoints();
+
+});
+
+
+
     //     map.addLayer(layer);
     // map.setView(HELSINKI, 19);
 
@@ -147,11 +145,3 @@ map.on('click', onMapClick);
     //         console.log(e);
     //         alert("Location access denied.");
     //     });
-
-window.highlightLayer = L.mapbox.featureLayer().addTo(map); 
-
-createPoints();
-
-
-
-});
