a36109780fbac0ffa112c0deb648fa99fb1a030d
end3rs-army <eric.m.dowty@gmail.com>
2015-06-10 10:45:02 -0600

    add vcr config stuff to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'capybara/rspec'
+require 'vcr'
 
 ActiveRecord::Migration.maintain_test_schema!
 
@@ -10,4 +11,9 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
+end
+
+VCR.configure do |config|
+ config.cassette_library_dir = 'spec/vcr_cassettes'
+ config.hook_into :webmock
 end
