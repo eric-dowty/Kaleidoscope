25beb0a181bd5336c9bff0f4cd5bf19181f083c5
end3rs-army <eric.m.dowty@gmail.com>
2015-06-11 05:17:54 -0600

    feature test

---
+++ spec/features/user_sees_root_path_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature "map page", type: :feature do
+
+  scenario "user visits homepage" do
+    VCR.use_cassette("get_denver_instagrams") do 
+      visit root_path
+
+      expect(page).to have_content("Point - Click - View")
+      expect(page).to have_content("See Most Recent Posts")
+      expect(page).to have_content("Find My Location")
+      expect(page).to have_content("Popular Tags")
+    end
+  end
+
+  scenario "user looks at instagram data" do
+  end
+
+
+end
